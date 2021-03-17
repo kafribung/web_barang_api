1b0bec52305a892007944dc6611ee00f46ea2d91
kafribung <60900117001@uin-alauddin.ac.id>
2021-03-17 21:28:09 +0800

    inventory table

---
+++ database/migrations/2021_03_17_125317_create_inventories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('stock_in');
+            $table->integer('stock_out')->default(0);
+            $table->double('price');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
