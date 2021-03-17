ea12041d5e1e15b94103781655202ef5a1c05d4d
kafribung <60900117001@uin-alauddin.ac.id>
2021-03-17 22:06:46 +0800

    loginRequest

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,14 +7,9 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function __invoke(Request $request)
     {
-        //
+        $data =  $request->only(['username', 'password']);
+        
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Backend\InventoryController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('login', LoginController::class);
 
 Route::get('inventory', [InventoryController::class, 'index']);
