9e021716a9db363686a3ac717ec21ce571079328
kafribung <60900117001@uin-alauddin.ac.id>
2021-03-17 21:58:25 +0800

    auth login controller

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\InventoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\InventoryController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
