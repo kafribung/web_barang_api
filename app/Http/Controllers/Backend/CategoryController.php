e0bb92ebc9a4015ccf822b1f52fa2d3ec2b54404
kafribung <60900117001@uin-alauddin.ac.id>
2021-04-02 20:15:27 +0800

    colection resource

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Backend\CategoryResource;
 use App\Models\Category;
-use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
